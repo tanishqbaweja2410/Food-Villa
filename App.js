e6f79d2ed771de6f55b1985a2944214b29af070a
Tanishq Baweja <tanishq.baweja001@gmail.com>
2023-01-26 13:30:50 +0530

    created-header

--- App.js
+++ App.js
@@ -1,24 +1,93 @@
-// const heading = React.createElement("h1", {}, "Hello World From React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// Nesting => we will try to implement this
-// <div>
-//     <h1>Akshay Saini</h1>
+// <--------------- nested structure using React.createElement() ------------------------->
+// const title = React.createElement(
+//   "div",
+//   {
+//     class: "title",
+//   },
+//   [
+//     React.createElement("h1", { id: "h1", key: "Large" }, "I'm a h1 tag"),
+//     React.createElement("h2", { id: "h2", key: "Medium" }, "I'm a h2 tag"),
+//     React.createElement("h3", { id: "h3", key: "Small" }, "I'm a h3 tag"),
+//   ]
+// );
+
+// <--------------- same structure using JSX ------------------------->
+// const title = (
+//   <div className="title">
+//     <h1 id="h1" key="Large">
+//       I'm a h1 tag
+//     </h1>
+//     <h2 id="h2" key="Medium">
+//       I'm a h2 tag
+//     </h2>
+//     <h3 id="h3" key="Small">
+//       I'm a h3 tag
+//     </h3>
+//   </div>
+// );
+
+// <--------------- functional component of the same structure ------------------------->
+// const Title = () => (
+//   <div className="title">
+//     <h1 id="h1" key="Large">
+//       I'm a h1 tag
+//     </h1>
+//     <h2 id="h2" key="Medium">
+//       I'm a h2 tag
+//     </h2>
+//     <h3 id="h3" key="Small">
+//       I'm a h3 tag
+//     </h3>
+//   </div>
+// );
+
+// <--------------- Composition Of Components ------------------------->
+// const TeacherInfo = () => {
+//   return (
 //     <div>
-//         <h2>Teacher</h2>
-//         <div>
-//             <img src="" alt="">
-//             <p></p>
-//         </div>
+//       <h1>Akshay Saini</h1>
+//       <TeacherImg>
+//       </TeacherImg>
 //     </div>
-// </div>
-const name = React.createElement("h1", {id: "name"}, "Akshay Saini");
-const designation = React.createElement("h2", {id: "teacher"}, "Teacher");
-const profilePic = React.createElement("img", {src: "https://yt3.ggpht.com/ytc/AMLnZu-cSoO0e2Shro4sYkaKYCumlgllLCTYYNf7bk7G_g=s900-c-k-c0x00ffffff-no-rj"}, null);
-const work = React.createElement("p", {id: "work"}, "Creator of Namaste JS");
-const about = React.createElement("div", {id: "about"}, [profilePic, work]);
-const info = React.createElement("div", {id: "info"}, [designation, about]);
-const container = React.createElement("div", {id: "container"}, [name, info]);
-root.render(container);
+//   );
+// };
 
+// <--------------- Header ------------------------->
+const Header = () => {
+  return (
+    <header className="container">
+      <Logo />
+      <SearchBar />
+      <UserIcon />
+    </header>
+  );
+};
+
+const Logo = () => {
+  return (
+    <img
+      className="logo"
+      src="https://namastedev.com/namaste-react-live/assets/img/small-logo.png"
+    />
+  );
+};
+
+const SearchBar = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Search" />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+const UserIcon = () => {
+  return <img className="user-icon" src="https://www.pngkey.com/png/full/72-729716_user-avatar-png-graphic-free-download-icon.png" alt="user" />;
+};
+
+// <--------------- Rendering ------------------------->
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
